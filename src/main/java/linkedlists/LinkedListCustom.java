26e5b26c04de72e99ad4c7b7b8244b55113a2f3a
deepeshy <programmer.dy@gmail.com>
2018-06-23 18:14:15 +0530

    linked list - first draft for adding, displaying and reversing

--- src/main/java/linkedlists/LinkedListCustom.java
+++ src/main/java/linkedlists/LinkedListCustom.java
@@ -2,56 +2,128 @@ package linkedlists;
 
 public class LinkedListCustom {
 
-    private Node head;
+  private Node head;
 
-    public static class Node {
-        int value;
-        Node next;
+  public static class Node {
 
-        public Node(int value) {
-            this.value = value;
-        }
+    int value;
+    Node next;
 
-        public void setNext(Node next) {
-            this.next = next;
-        }
-
-        public int getValue() {
-            return value;
-        }
-
-        public Node getNext() {
-            return next;
-        }
+    public Node(int value) {
+      this.value = value;
     }
 
-    public void add(int val) {
-        if (head == null) {
-            head = new Node(val);
-            head.setNext(null);
-        } else {
-
-        }
+    public void setNext(Node next) {
+      this.next = next;
     }
 
-    public void remove(int val) {
-
+    public int getValue() {
+      return value;
     }
 
-    public void display() {
-        Node next = this.head;
-        while (next != null) {
-            System.out.print(next.getValue());
-            System.out.print(" ");
-            next = next.getNext();
-        }
+    public Node getNext() {
+      return next;
+    }
+  }
+
+  public void addHead(int val) {
+    if (head == null) {
+      initializeLinkedList(val);
+
+    } else {
+      Node rootCopy = new Node(head.value);
+      rootCopy.next = head.next;
+      Node newNode = new Node(val);
+      newNode.next = rootCopy;
+      head = newNode;
+    }
+  }
+
+  public void addTail(int val) {
+    if (head == null) {
+      initializeLinkedList(val);
+    } else {
+      Node n = head;
+      while (n.next != null) {
+        n = n.next;
+      }
+      n.next = new Node(val);
+      n.next.next = null;
+    }
+  }
+
+  private void initializeLinkedList(int val) {
+    head = new Node(val);
+    head.next = null;
+  }
+
+
+  public void remove(int val) {
+
+  }
+
+  public void display() {
+    Node next = this.head;
+    while (next != null) {
+      System.out.print(next.getValue());
+      System.out.print("->");
+      next = next.getNext();
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    LinkedListCustom linkedListCustom = new LinkedListCustom();
+    linkedListCustom.addHead(10);
+    linkedListCustom.addHead(12);
+    linkedListCustom.addHead(3);
+    linkedListCustom.display();
+
+    LinkedListCustom linkedListCustom2 = new LinkedListCustom();
+    linkedListCustom2.addTail(10);
+    linkedListCustom2.addTail(12);
+    linkedListCustom2.addTail(3);
+    linkedListCustom2.addTail(32);
+    linkedListCustom2.addTail(35);
+    linkedListCustom2.display();
+    linkedListCustom.reverse();
+    linkedListCustom2.reverse();
+
+    linkedListCustom.display();
+    linkedListCustom2.display();
+
+    LinkedListCustom linkedListCustomOneElement = new LinkedListCustom();
+    linkedListCustomOneElement.addTail(10);
+    linkedListCustomOneElement.reverse();
+    linkedListCustomOneElement.display();
+
+    LinkedListCustom linkedListCustomTwoElement = new LinkedListCustom();
+    linkedListCustomTwoElement.addTail(10);
+    linkedListCustomTwoElement.addTail(13);
+    linkedListCustomTwoElement.reverse();
+    linkedListCustomTwoElement.display();
+
+  }
+
+  public void reverse() {
+    if (head == null || head.next == null) {
+      return;
     }
 
-    public static void main(String[] args) {
-        LinkedListCustom linkedListCustom = new LinkedListCustom();
-        linkedListCustom.add(10);
-        linkedListCustom.add(12);
-        linkedListCustom.add(3);
-    }
+    Node prev = head;
+    Node curr = head.next;
+    Node nxt = curr.next;
+    prev.next = null;
 
+    while (curr != null) {
+      curr.next = prev;
+      prev = curr;
+      head = curr;
+      curr = nxt;
+
+      if (nxt != null) {
+        nxt = nxt.next;
+      }
+    }
+  }
 }
