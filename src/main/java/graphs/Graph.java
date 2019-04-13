2b7f7e63f3b7ec8f736cf8a581cbf189ff64eee7
deepeshy <programmer.dy@gmail.com>
2019-04-13 22:44:05 +0530

    Graph with dfs using recursion and using stack

---
+++ src/main/java/graphs/Graph.java
@@ -0,0 +1,81 @@
+package graphs;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class Graph {
+
+  private int V;
+  private List<Integer>[] edges;
+
+  public Graph(int i) {
+    V = i;
+    edges = (ArrayList<Integer>[]) (new ArrayList[V]);
+    for (int j = 0; j < V; j++) {
+      edges[j] = new ArrayList<>();
+    }
+
+  }
+
+  private void addEdge(int node1, int node2) {
+    edges[node1].add(node2);
+    edges[node2].add(node1);
+  }
+
+  private void dfs(int startingNode) {
+    boolean visited[] = new boolean[V];
+    dfs(startingNode, visited);
+  }
+
+  private void dfs(int i, boolean visited[]) {
+    if (!visited[i]) {
+      visited[i] = true;
+      System.out.print(i + "->");
+      for (int neighbor : edges[i]) {
+        dfs(neighbor, visited);
+      }
+    }
+  }
+
+
+  private void dfsStack(int origin) {
+    Deque<Integer> stack = new ArrayDeque<>();
+    stack.push(origin);
+    boolean visited[] = new boolean[V];
+    while (!stack.isEmpty()) {
+      Integer current = stack.pop();
+      if (!visited[current]) {
+        System.out.print(current + "->");
+        visited[current] = true;
+      }
+
+      for (int i : edges[current]) {
+        if (!visited[i]) {
+          stack.push(i);
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Graph graph = new Graph(10);
+    graph.addEdge(1, 2);
+    graph.addEdge(1, 9);
+    graph.addEdge(2, 3);
+    graph.addEdge(2, 8);
+    graph.addEdge(3, 4);
+//    graph.addEdge(3, 5);
+//    graph.addEdge(5, 1);
+    graph.addEdge(5, 7);
+    graph.addEdge(5, 9);
+    graph.addEdge(5, 0);
+    graph.addEdge(5, 2);
+
+    System.out.println("Done");
+    graph.dfsStack(1);
+    System.out.println();
+    graph.dfs(1);
+  }
+}
